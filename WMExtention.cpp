ce01e6cb6ebbabecd7954631440cceb357ba95d5
Denis <radiontcev@ivit.pro>
2021-03-16 11:24:08 +0300

    fix warning in newAction()

--- WMExtention.cpp
+++ WMExtention.cpp
@@ -13,7 +13,7 @@ void WMExtention::init(void (*function)(void))
 void WMExtention::newAction(unsigned char *d)
 {
     // load button data from user application
-    memcpy(twi_reg, d, 8);
+    memcpy((void*)twi_reg, d, 8);
 }
 
 unsigned char WMExtention::getReg(unsigned char addr)
